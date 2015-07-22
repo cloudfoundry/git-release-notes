fab049879759cf0b450673ef07f63764960f39f9
Amit Kumar Gupta <agupta@pivotal.io>
2015-07-22 11:25:35 -0700

    Update CLI banners

--- lib/git_release_notes/cli.rb
+++ lib/git_release_notes/cli.rb
@@ -7,25 +7,24 @@ module GitReleaseNotes
 		 "including commits within submodules"
     method_option :from,
 		   desc: "SHA, tag, or other tree-ish reference for the start of the range",
-		   banner: "SHA, tag, or other tree-ish reference for the start of the range",
+		   banner: "TREEISH",
 		   type: :string,
 		   required: true,
 		   aliases: "-f"
     method_option :to,
 		   desc: "SHA, tag, or other tree-ish reference for the end of the range",
-		   banner: "SHA, tag, or other tree-ish reference for the end of the range",
+		   banner: "TREEISH",
 		   type: :string,
 		   required: true,
 		   aliases: "-t"
     method_option :git_web_url,
 		   desc: "URL of the root repository, used to create links in the generated HTML doc",
-		   banner: "URL of the root repository, used to create links in the generated HTML doc",
+		   banner: "URL",
 		   type: :string,
 		   default: "https://github.com/cloudfoundry/cf-release",
 		   aliases: "-u"
     method_option :exclude_submodules_without_changes,
 		   desc: "Exclude showing submodules in HTML doc if there are 0 commits to it in the given range",
-		   banner: "Exclude showing submodules in HTML doc if there are 0 commits to it in the given range",
 		   type: :boolean,
 		   default: false,
 		   aliases: "-e"
